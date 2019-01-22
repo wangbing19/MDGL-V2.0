c2807b507fc563f01c11a13006089916a7582267
‘e <251523891@qq.com>
2019-01-22 12:53:08 +0800

    李宏达代言

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Helllo World!");
-        System.out.println("李宏达是傻逼");
+        System.out.println("李宏达");
     }
 }
