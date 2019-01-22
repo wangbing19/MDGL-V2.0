102798dd02c8fa0590516c0bf91729ed1d695283
lxhn <45892806+lxhn@users.noreply.github.com>
2019-01-22 22:27:10 +0800

    1
    vision

---
+++ vision-parent/vision-web/src/main/java/vision/VisionWeb.java
@@ -0,0 +1,14 @@
+package vision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class VisionWeb {
+
+    public static void main(String[] args){
+        SpringApplication.run(VisionWeb.class,args);
+    }
+
+}
---
+++ vision-parent/vision-web/src/main/java/vision/visionweb/contorller/WebController.java
@@ -0,0 +1,15 @@
+package vision.visionweb.contorller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WebController {
+
+    @RequestMapping("/find")
+ //   @ResponseBody
+    public String find(){
+        return "pages/starter";
+    }
+
+}
