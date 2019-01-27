efeef209d7f8abd20c735b3eaa8a9c4e3a55f024
wangbing92 <448255517@qq.com>
2019-01-27 14:21:27 +0800

    1

--- vision-parent/vision-api/src/main/java/com/vs/vision/pojo/ppo/PpoAppointment.java
+++ vision-parent/vision-api/src/main/java/com/vs/vision/pojo/ppo/PpoAppointment.java
@@ -17,7 +17,7 @@ public class PpoAppointment {
      * 主键ID
      */
     private Long id;
-    private Integer usre_id;
+    private Integer usreId;
     private String trainerName;
     private String gender;
     private String tel;
